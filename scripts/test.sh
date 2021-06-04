4c71a337e07d41ef2ac586fdea71e5b31947995c
david <davidpj@hotmail.co.uk>
2021-06-04 15:30:51 +0000

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ sudo apt-get install python3-venv -y
 
 python3 -m venv venv
 source venv/bin/activate
-pip3 install -r frontend/requirements.txt
+pip3 install -r requirements.txt
 pip3 install requests-mock
 
 
