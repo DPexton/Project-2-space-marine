3b4db0086d27cc35dddfb96d6e8f9911cccbf4f3
david <davidpj@hotmail.co.uk>
2021-06-04 12:46:32 +0000

    updated task

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 docker --version
-docker-compose down --rmi all
 docker login -u ${docker_credentials_USR} -p ${docker_credentials_PSW}
-docker-compose build
+docker-compose build --parallel
 docker-compose push 
