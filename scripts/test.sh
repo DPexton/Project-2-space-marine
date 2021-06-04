c2553023ef506e1e757736323a724f5854c25bd8
david <davidpj@hotmail.co.uk>
2021-06-04 08:29:10 +0000

    updated tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,8 @@
 #!/bin/bash
 
 sudo apt update 
-sudo apt install python3 python3-pip python3-venv
+sudo apt install python3 python3-pip 
+sudo apt install python3-venv
 
 python3 -m venv venv
 source venv/bin/activate
