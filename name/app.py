6e462e2b667151eea6d52c6f117ab506d0e2b142
david <davidpj@hotmail.co.uk>
2021-06-05 12:10:34 +0000

    updated testing

--- name/app.py
+++ name/app.py
@@ -21,6 +21,8 @@ def name():
             name = "Gunbjorn Ironmaw"
         elif role == "Scout Marine":
             name = "One-Eye Frodi"
+        elif role == "Terminator Marine"
+            name = "Tyreous Hengun"
     
     elif chapter == "Ultramarines":
         if role == "Tactical Marine":
@@ -31,6 +33,8 @@ def name():
             name = "Jonah Sidonis"
         elif role == "Scout Marine":
             name = "Barachiel Sadros"
+        elif role == "Terminator Marine"
+            name = "Aecon Evidist"
     
     elif chapter == "Imperial Fists":
         if role == "Tactical Marine":
@@ -41,6 +45,9 @@ def name():
             name = "Klordath Aglibesco"
         elif role == "Scout Marine":
             name = "Kazryn Batariar"
+        elif role == "Terminator Marine"
+            name = "Kazrago Hannibia"
+    
     
     elif chapter == "Dark Angels":
         if role == "Tactical Marine":
@@ -51,7 +58,32 @@ def name():
             name = "Lutheon Alcane"
         elif role == "Scout Marine":
             name = "Cassias Gabrun"
-    
+        elif role == "Terminator Marine":
+            name = "Anaticus Redlici"
+
+    elif chapter == "Salamanders":
+        if role == "Tactical Marine":
+            name = "Belial Iasiar"
+        elif role == "Assault Marine":
+            name = "Varreon Baaloss"
+        elif role == "Devastator Marine":
+            name = "Sappheran Petrail"
+        elif role == "Scout Marine":
+            name = "Ophaniel Salatis"
+        elif role == "Terminator Marine":
+            name = "Maximiz Zephit"  
+        
+    elif chapter == "Blood Angels":
+        if role == "Tactical Marine":
+            name = "Beler Antilochiad"
+        elif role == "Assault Marine":
+            name = "Azazel Annellurius"
+        elif role == "Devastator Marine":
+            name = "Ubaldun Chronal"
+        elif role == "Scout Marine":
+            name = "Azreal Berian"
+        elif role == "Terminator Marine":
+            name = "Sarathiel Asbeo"  
     else: 
         return "Please Press Again"
 
--- name/testing/test_4.py
+++ name/testing/test_4.py
@@ -14,19 +14,33 @@ class TestResponse(TestBase):
        'Space Wolves,Tactical Marine': b"Grimolf Ulfsson",
        'Space Wolves,Assault Marine': b"Ragnar Blackmane",
        'Space Wolves,Devastator Marine': b"Gunbjorn Ironmaw",
-       'Space Wolves,Scout Marine': b"One-Eye Frodi",
+       'Space Wolves,Scout Marine': b"One-Eye Frodi"
+       'Space Wolves,Terminator Marine': b"Tyreous Hengun",
        'Ultramarines,Tactical Marine': b"Titus Grimaldus",
        'Ultramarines,Assault Marine': b"Maximus Tarimus",
        'Ultramarines,Devastator Marine': b"Jonah Sidonis",
        'Ultramarines,Scout Marine': b"Barachiel Sadros",
+       'Ultramarines,Terminator Marine': b"Aecon Evidist",
        'Imperial Fists,Tactical Marine': b"Shal Cestros",
        'Imperial Fists,Assault Marine': b"Uziel Aryabhon",
        'Imperial Fists,Devastator Marine': b"Klordath Aglibesco",
        'Imperial Fists,Scout Marine': b"Kazryn Batariar",
+       'Imperial Fists,Terminator Marine': b"Kazrago Hannibia",
        'Dark Angels,Tactical Marine': b"Baelar Sadross",
        'Dark Angels,Assault Marine': b"Gabriel Manuzanus",
        'Dark Angels,Devastator Marine': b"Lutheon Alcane",
-       'Dark Angels,Scout Marine': b"Cassias Gabrun"
+       'Dark Angels,Scout Marine': b"Cassias Gabrun",
+       'Dark Angels,Terminator Marine': b"Anaticus Redlici",
+       'Salamanders,Tactical Marine': b"Belial Iasiar",
+       'Salamanders,Assault Marine': b"Varreon Baaloss",
+       'Salamanders,Devastator Marine': b"Sappheran Petrail",
+       'Salamanders,Scout Marine': b"Ophaniel Salatis"
+       'Salamanders,Terminator Marine': b"Maximiz Zephit",
+       'Blood Angels,Tactical Marine': b"Beler Antilochiad",
+       'Blood Angels,Assault Marine': b"Azazel Annellurius",
+       'Blood Angels,Devastator Marine': b"Ubaldun Chronal",
+       'Blood Angels,Scout Marine': b"Azreal Berian",
+       'Blood Angels,Terminator Marine': b"Sarathiel Asbeo"
     }
     def test_spacewolves1(self):
         for data,result in self.test_cases.items():
